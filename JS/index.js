72f105a176bc72f18c054eac52fd61b536442cfd
leandrofn3 <lf1938833@gmail.com>
2023-05-30 22:20:08 -0300

    finalização da barra de pesquisa

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,6 @@
 window.onload = () => {
     getcharacters()
+
 }
 
 const url = "https://rickandmortyapi.com/api"
@@ -18,47 +19,23 @@ function getcharacters() {
                         <img src="${character.image}" >
                         <div class="textCards">
                             <h2>${character.name}</h2>
-                            <p class="pText">${character.status} - ${character.species}</p>
+                            <p class="pTextStatus"><p class="circle"></p>${character.status} - ${character.species}</p>
                             <p class="pTitulo">last known location:</p>
                             <p class="pText">${character.location.name}</p>
-                            
                         </div>
                 </div>`
+
+                // if (character.status === "Alive") {
+                //     statusCor = "#56CD42";
+                //     statusTexto = "Vivo";
+                // } else if (character.status === "Dead") {
+                //     statusCor = "#CD4242";
+                //     statusTexto = "Morto";
+                // } else {
+                //     statusCor = "#BBBBBB";
+                //     statusTexto = "Desconhecido";
+                // }
             });
         })
         .catch(error => console.log(error))
-
-    function buscarPersonagem(data) {
-        const pesquisar = document.getElementById('perquisar').value;
-        let cards = document.getElementById("cardsContainer");
-        cards.innerHTML = ""
-        const nameCharacter = data.filter(character => character.name.toLowerCase() === pesquisar.toLowerCase())
-        cards.innerHTML = `<div>
-                        <img src="${nameCharacter.image}" >
-                        <div class="textCards">
-                            <h2>${nameCharacter.name}</h2>
-                            <p class="pText">${nameCharacter.status} - ${nameCharacter.species}</p>
-                            <p class="pTitulo">last known location:</p>
-                            <p class="pText">${nameCharacter.location.name}</p>
-                            
-                        </div>
-            </div>`
-    }
-
 }
-
-// mudar o formulario tirar e colocar em uma div
-// fazer o filtro, estruturar
-
-
-// function oneCharcters (){
-//     api.get(`/character/3`)
-//     .then(response => {
-//         const data = response.data
-//         document.getElementById("cardsContainer").innerHTML = JSON.stringify(data);
-//     })
-//     .catch(error => console.log(error))
-// }
-
-// oneCharcters()
-
